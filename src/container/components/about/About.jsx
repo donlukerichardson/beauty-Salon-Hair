965541e13cbe31b0a5a59b9c2f2a0c9a60a34b18
albouhajiDev <albouhajidev@gmail.com>
2022-05-07 16:07:24 +0000

    new

--- src/container/components/about/About.jsx
+++ src/container/components/about/About.jsx
@@ -3,8 +3,10 @@ import React from "react";
 function About() {
   return (
     <>
-      <section className="about" id="about" 
-      style={{backgroundImage: `url(./img/arabesque-left-bottom.svg)`}}
+      <section
+        className="about"
+        id="about"
+        style={{ backgroundImage: `url(./img/arabesque-left-bottom.svg)` }}
       >
         <div className="container">
           <div className="section-title">
@@ -18,16 +20,11 @@ function About() {
               </div>
               <div className="about-description">
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.Velit
-                  possimus a sint
-                </p>
-                <p>
-                  Obcaecati porro accusamus iusto rem aperiam. Lorem ipsum dolor
-                  sit amet consectetur adipisicing elit. Velit possimus a sint
-                </p>
-                <p>
-                  Eligendi nostrum architecto quibusdam. Neque odio enim quasi
-                  ad culpa similique quas.
+                  Un coiffeur ou une coiffeuse est une personne formée pour
+                  couper et entretenir les cheveux. Elle peut travailler dans un
+                  salon de coiffure, à son domicile ou chez son client. Les
+                  types de coiffures, les techniques et les outils employés sont
+                  nombreux et varient selon le mandat confié au coiffeur.
                 </p>
               </div>
             </div>
